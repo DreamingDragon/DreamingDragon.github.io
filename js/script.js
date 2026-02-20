a8f340d9b01cbc715331daf5283ffa376c0336f1
Vincent Liu <Crystalmew223@gmail.com>
2026-02-19 22:34:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function incrementSlotNumber(slot) {
   // ✅ Re-enable clicks after full flip duration
   setTimeout(() => {
     slot.style.pointerEvents = "auto";
-  }, 600); // matches 0.2s + 0.2s animation
+  }, 450); // matches 0.2s + 0.2s animation
 }
 
 function calculateMachineOutput() {
